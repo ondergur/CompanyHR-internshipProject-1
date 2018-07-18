ffaa18a2d4e4eb091311305fcac3bea891ad3eb8
ondergur <ahmetondergur195@gmail.com>
2018-07-18 11:50:27 +0300

    company phone type changed

--- database/migrations/2018_07_17_083017_create_companies_table.php
+++ database/migrations/2018_07_17_083017_create_companies_table.php
@@ -17,7 +17,7 @@ class CreateCompaniesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('address');
-            $table->integer('phone');
+            $table->string('phone');
             $table->string('email');
             $table->string('website');
             $table->timestamps();
