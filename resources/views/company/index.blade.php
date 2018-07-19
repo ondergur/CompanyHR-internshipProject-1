f5a04ac78372635b62c30d35a6579f0fa05cd863
ondergur <ahmetondergur195@gmail.com>
2018-07-19 10:42:38 +0300

    company index view style changes

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,20 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Company Index</h1>
-    <ul>
+    {{--<h1 style="margin: 0px; padding-left: 40px; padding-bottom: 16px">Company Index</h1>--}}
+    <h1 class="baslik">Company Index</h1>
         @if(count($companies))
-            @foreach($companies as $company)
-                {{--<li> <a href="/company/{{$company->id}}">{{ $company->name }} </a> </li>--}}
-                <li> <a href="{{url('companies/'.$company->id.'/edit')}}">{{$company->name}}</a> </li>
-            @endforeach
+            <div class="row">
+                @foreach($companies as $company)
+                    <div class="col-sm-3">
+                        {{--<li> <a href="/company/{{$company->id}}">{{ $company->name }} </a> </li>--}}
+                        <div class="card" style="">
+                            <img class="card-img-top" src="{{asset('company_logos/'.$company->logo)}}" alt="{{$company->logo.' logo'}}">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <a href="{{url('companies/'.$company->id)}}">{{$company->name}}</a>
+                                </h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="{{url('companies/'.$company->id)}}" class="btn btn-primary">Company Information</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         @else
 
             <p>You don't have any Company!</p>
 
         @endif
-    </ul>
-    {{ $companies->links() }}
-    <a href="{{ url('companies/create') }}" class="btn btn-primary">Create New Company!</a>
+    <div style="padding-left: 40px; padding-top: 16px"> {{ $companies->links() }} </div>
+    <a href="{{ url('companies/create') }}" class="btn btn-primary" style="margin-left: 40px">Create New Company!</a>
 
 @endsection
