6e32e4f0a34f0b9b0f279b32fb30406372ee67d4
ondergur <ahmetondergur195@gmail.com>
2018-07-23 17:08:53 +0300

    employee show.blade added

---
+++ resources/views/employee/show.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card" style="">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="{{route('employees.show', $employee)}}">{{$employee->name}} {{$employee->lastname}}</a>
+                        </h5>
+                        <p class="card-text">
+                            Phone: {{$employee->phone}}
+                        </p>
+                        <p class="card-text">
+                            E-mail: <a href="mailto:{{$employee->mail}}">{{$employee->email}}</a>
+                        </p>
+                        <p class="card-text">
+                            Company: {{$employee->companyid}}
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="float-left">
+                            <a href="{{route('employees.edit', $employee)}}" class="btn btn-primary">Edit Employee</a>
+                        </div>
+                        <div class="float-right">
+                            {{Form::open([ 'method' => 'delete', 'route' => ['employees.destroy', $employee]])}}
+                            {{Form::submit('Delete Employee', array('class' => 'btn btn-danger justify-content-end')) }}
+                            {{Form::close() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
