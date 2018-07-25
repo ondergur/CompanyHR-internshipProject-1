507aa20a8f4e63fcead8fd3c7767b8516d48b77f
ondergur <ahmetondergur195@gmail.com>
2018-07-25 11:48:03 +0300

    #

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -14,6 +14,7 @@ class CompanyController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param Request $request
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
